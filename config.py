0a055d4b4411281b41f483e92f8e00dbbe48ecd5
Blayne Bennett <blayneb@Blaynes-MacBook-Pro.local>
2020-08-10 09:33:26 -0700

    added explore page and pagination to front and backend objects

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -13,3 +12,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['blaynebennett199@gmail.com']
+
+    POSTS_PER_PAGE = 25
